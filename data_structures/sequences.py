3529a64aaa617a239e06288d2ebbc0ca373b7472
Maaxboon <maaxwelokoth@gmail.com>
2024-08-21 00:20:31 +0300

    Adds sequences

---
+++ data_structures/sequences.py
@@ -0,0 +1,15 @@
+# Sequences: String, List, Tuple
+
+# Indexing: access any item in the sequence using its index. It starts with 0 for the first element.
+# string
+
+x = 'frog'
+print (x[3])
+
+# list
+x = ['pig', 'cow', 'horse']
+print (x[1])
+
+# tuple
+x = ('Kevin', 'Niklas', 'Jenny', 'Craig')
+print (x[0])
