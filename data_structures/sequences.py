e5507ea68f3428f9658ca75284e8a5da408d753f
Maaxboon <maaxwelokoth@gmail.com>
2024-08-21 00:47:58 +0300

    Adds slicing

--- data_structures/sequences.py
+++ data_structures/sequences.py
@@ -1,8 +1,11 @@
 # Sequences: String, List, Tuple
 
 # Indexing: access any item in the sequence using its index. It starts with 0 for the first element.
-# string
 
+# Slicing: slice out substrings, sublists, subtuples using indexes
+# [start : end+1 : step]
+
+# string
 x = 'frog'
 print (x[3])
 
@@ -13,3 +16,14 @@ print (x[1])
 # tuple
 x = ('Kevin', 'Niklas', 'Jenny', 'Craig')
 print (x[0])
+
+# Slicing
+
+x = 'computer'
+print(x[1:4])
+print(x[1:6:2])
+print(x[3:])
+print(x[:5])
+print(x[-1])
+print(x[-3:])
+print(x[:-2])
