aa907b1fb5656f84d32af37ffff3009637d2e008
Maaxboon <maaxwelokoth@gmail.com>
2024-08-26 00:42:46 +0300

    returns count of an item

---
+++ data_structures/count.py
@@ -0,0 +1,11 @@
+# string
+x = 'hippo'
+print(x.count('p'))
+
+# list 
+y = ['cow', 'pig', 'horse', 'cow']
+print(y.count('cow'))
+
+# tuple
+z = ('Kevin', 'Niklas', 'Jenny', 'Craig')
+print(z.count('Kevin'))
