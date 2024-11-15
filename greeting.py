3dd8e4a0ac1e5a637aec282030a94f8af7d701ba
Maaxboon <maaxwelokoth@gmail.com>
2024-11-15 11:08:33 +0300

    Adds greeting

---
+++ greeting.py
@@ -0,0 +1,8 @@
+# store the user's name in a variable
+userName = 'Maaxboon'
+
+# Create a message using the name
+greeting = 'Hello, ' + userName + '!'
+
+# Display the greeting on the screen
+print(greeting)
