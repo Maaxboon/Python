96e149228f364e3507b8534a800aff339d81c1d3
Maaxboon <maaxwelokoth@gmail.com>
2025-01-03 18:17:43 +0300

    Removes deleted files

--- data_structures/Sequences/sorting2.py
+++
@@ -1,4 +0,0 @@
-# Add a key prameter and a lambda function to return the second character
-
-z = ('Kevin', 'Niklas', 'Jenny', 'Craig')
-print(sorted(z, key=lambda k:k[1]))
--- greeting.py
+++
@@ -1,8 +0,0 @@
-# store the user's name in a variable
-userName = 'Maaxboon'
-
-# Create a message using the name
-greeting = 'Hello, ' + userName + '!'
-
-# Display the greeting on the screen
-print(greeting)
